001758e0fe202a05c911b3230f464b86d91729c3
FlavSallMaix <fla.sm@orange.fr>
2025-05-12 16:34:30 +0200

    try to make mistral ai work

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+
+import "./style.css";
+
+const Home = () => {
+  return (
+    <div id="home">
+      <main></main>
+    </div>
+  );
+};
+
+export default Home;
